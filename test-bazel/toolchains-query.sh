ebfe1055d6f04d02b3e59db1a16d6b73bd12189f
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2025-07-07 08:21:48 +0200

    Hide error output

--- test-bazel/toolchains-query.sh
+++ test-bazel/toolchains-query.sh
@@ -6,12 +6,12 @@ SCRIPT_DIR="$(dirname "$0")"
 echo "\n"
 echo "\n"
 echo "--output build"
-bazel cquery --output build "@bazel_tools//tools/jdk:current_java_toolchain + @bazel_tools//tools/jdk:current_java_runtime"
+bazel cquery --output build "@bazel_tools//tools/jdk:current_java_toolchain + @bazel_tools//tools/jdk:current_java_runtime" 2>/dev/null
 
 echo "\n"
 echo "\n"
 echo "--output starlark"
-bazel cquery --output starlark --starlark:file $SCRIPT_DIR/toolchains-query-format.scl "@bazel_tools//tools/jdk:current_java_toolchain + @bazel_tools//tools/jdk:current_java_runtime"
+bazel cquery --output starlark --starlark:file $SCRIPT_DIR/toolchains-query-format.scl "@bazel_tools//tools/jdk:current_java_toolchain + @bazel_tools//tools/jdk:current_java_runtime" 2>/dev/null
 
 echo "\n"
 echo "\n"
