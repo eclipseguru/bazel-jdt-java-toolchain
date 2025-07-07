5acd3fb15645f9e0ef2cf9686d287952d7680951
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2025-07-07 08:02:41 +0200

    Add repo for possible cquery issue

---
+++ test-bazel/toolchains-query.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+SCRIPT_DIR="$(dirname "$0")"
+
+echo "\n"
+echo "\n"
+echo "--output build"
+bazel cquery --output build "@bazel_tools//tools/jdk:current_java_toolchain + @bazel_tools//tools/jdk:current_java_runtime"
+
+echo "\n"
+echo "\n"
+echo "--output starlark"
+bazel cquery --output starlark --starlark:file $SCRIPT_DIR/toolchains-query-format.scl "@bazel_tools//tools/jdk:current_java_toolchain + @bazel_tools//tools/jdk:current_java_runtime"
+
+echo "\n"
+echo "\n"
