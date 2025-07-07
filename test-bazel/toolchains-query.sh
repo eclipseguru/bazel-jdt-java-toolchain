797130bcbe7696bdff8cd4bb8f1e7ef939717e0a
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2025-07-07 10:49:31 +0200

    Add more output to test script

--- test-bazel/toolchains-query.sh
+++ test-bazel/toolchains-query.sh
@@ -10,6 +10,11 @@ bazel shutdown
 
 echo "\n"
 echo "\n"
+echo "cquery"
+bazel cquery "@bazel_tools//tools/jdk:current_java_toolchain + @bazel_tools//tools/jdk:current_java_runtime" 2>/dev/null
+
+echo "\n"
+echo "\n"
 echo "--output build"
 bazel cquery --output build "@bazel_tools//tools/jdk:current_java_toolchain + @bazel_tools//tools/jdk:current_java_runtime" 2>/dev/null
 
@@ -27,3 +32,9 @@ echo "\n"
 echo "\n"
 echo "--output starlark"
 bazel cquery --output starlark --starlark:file $SCRIPT_DIR/toolchains-query-format.scl "@bazel_tools//tools/jdk:current_java_toolchain + @bazel_tools//tools/jdk:current_java_runtime" 2>/dev/null
+
+echo "\n"
+echo "\n"
+echo "cquery"
+bazel cquery "@bazel_tools//tools/jdk:current_java_toolchain + @bazel_tools//tools/jdk:current_java_runtime" 2>/dev/null
+
