fd9967af7c65c47dc7f5c11ab81dc8a7f542afd5
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2025-07-07 08:42:03 +0200

    Reproduce after shutdown

--- test-bazel/toolchains-query.sh
+++ test-bazel/toolchains-query.sh
@@ -5,6 +5,11 @@ SCRIPT_DIR="$(dirname "$0")"
 
 echo "\n"
 echo "\n"
+echo "shutdow"
+bazel shutdown
+
+echo "\n"
+echo "\n"
 echo "--output build"
 bazel cquery --output build "@bazel_tools//tools/jdk:current_java_toolchain + @bazel_tools//tools/jdk:current_java_runtime" 2>/dev/null
 
@@ -15,3 +20,10 @@ bazel cquery --output starlark --starlark:file $SCRIPT_DIR/toolchains-query-form
 
 echo "\n"
 echo "\n"
+echo "build something"
+bazel build //compiler:ecj //compiler:buildjar //compiler:bazel_org_jacoco_core //compiler:work_request_handlers //:JdtJavaBuilder
+
+echo "\n"
+echo "\n"
+echo "--output starlark"
+bazel cquery --output starlark --starlark:file $SCRIPT_DIR/toolchains-query-format.scl "@bazel_tools//tools/jdk:current_java_toolchain + @bazel_tools//tools/jdk:current_java_runtime" 2>/dev/null
